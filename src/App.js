1e312471164ea201df31835637151da533d2d3ac
Olatunji Oluwapelumi Samson <samsontunj@gmail.com>
2021-07-11 22:11:55 +0100

    turning regular function to arrow function

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import React from "react";
 import Card from "./components/Card";
 import emojilopedia from "./emojilopedia.js";
 
-function createEmojiCard(lopedia) {
-   return (
-      <Card
-         key={lopedia.id}
-         id={lopedia.id}
-         symbol={lopedia.symbol}
-         name={lopedia.name}
-         desc={lopedia.description}
-      />
-   );
-}
-
 function App() {
    return (
       <div>
@@ -21,9 +9,45 @@ function App() {
             <span>Emojilopedia</span>
          </h1>
 
-         <dl className="dictionary">{emojilopedia.map(createEmojiCard)}</dl>
+         <dl className="dictionary">
+            {emojilopedia.map((lopedia) => {
+               return (
+                  <Card
+                     key={lopedia.id}
+                     id={lopedia.id}
+                     symbol={lopedia.symbol}
+                     name={lopedia.name}
+                     desc={lopedia.description}
+                  />
+               );
+            })}
+         </dl>
       </div>
    );
 }
+
+// function createEmojiCard(lopedia) {
+//    return (
+//       <Card
+//          key={lopedia.id}
+//          id={lopedia.id}
+//          symbol={lopedia.symbol}
+//          name={lopedia.name}
+//          desc={lopedia.description}
+//       />
+//    );
+// }
+
+// function App() {
+//    return (
+//       <div>
+//          <h1>
+//             <span>Emojilopedia</span>
+//          </h1>
+
+//          <dl className="dictionary">{emojilopedia.map(createEmojiCard)}</dl>
+//       </div>
+//    );
+// }
 
 export default App;
